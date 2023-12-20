10cc159a691a05382ff374b1840754f6c7d74d02
Brian Corder <bjcorder@protonmail.com>
2023-12-20 13:46:54 -0600

    style: Add separate header and footer with branding

---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+    <h1>
+        <img src="/img/chaos-chat-high-resolution-logo-transparent.png" alt="Chaos-Chat Logo">
+    </h1>
+</header>
+
+<style>
+    header{
+        padding: 20px;
+    }
+    
+    h1{
+        text-align: center;
+    }
+    img{
+        max-width: 400px;
+    }
+</style>
